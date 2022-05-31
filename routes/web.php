e7dfed534ca6ba023f889d3473777e9eb7eb5b29
Ruben Kubalyan <53543691+RubenK800@users.noreply.github.com>
2022-05-31 11:29:07 +0400

    helloandyes

--- routes/web.php
+++ routes/web.php
@@ -17,5 +17,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+
 Route::any('/',function (){return view('welcome');});
 
+Route::any('/poster',function (){return view('approve');});
